a465bcea6067ae2dda36e9e11a42e980dce701d3
Aleksei Margolin <margolin96@gmail.com>
2021-10-23 01:03:05 +0300

    Fix

--- app.js
+++ app.js
@@ -96,12 +96,13 @@ setInterval(() => {
 // openssl genrsa -out privatekey.pem 2048
 // openssl req -new -key privatekey.pem -out certrequest.csr
 // openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
-const options = {
-  key  : fs.readFileSync(path.join(__dirname, 'certs/privatekey.pem')),
-  cert : fs.readFileSync(path.join(__dirname, 'certs/certificate.pem')),
-};
+// const options = {
+//   key  : fs.readFileSync(path.join(__dirname, 'certs/privatekey.pem')),
+//   cert : fs.readFileSync(path.join(__dirname, 'certs/certificate.pem')),
+// };
 
 // Create our HTTPS server listening on port 3000.
 const PORT = process.env.PORT || 5000;
-https.createServer(options, app)
+// https.createServer(options, app)
+app
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
