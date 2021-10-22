9cbb25d02e413bedfc3d45560fd13e7142d32dee
Aleksei Margolin <margolin96@gmail.com>
2021-10-22 23:20:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// app.get('/auth', (req, res) => {
+//   res.send('test');
+// })
+
+
+const bodyParser = require('body-parser');
+const OAuthServer = require('express-oauth-server');
+
+app.oauth = new OAuthServer({
+  model: {
+    getClient(clientId, clientSecret) {
+      console.log('getClient');
+      return {
+        id: '1',
+        redirectUris: ['https://localhost:5001'],
+        grants: ['authorization_code']
+      };
+    },
+    saveAuthorizationCode() {
+      console.log('saveAuthorizationCode');
+    },
+    getAccessToken() {
+      console.log('getAccessToken');
+    }
+  }
+});
+
+app.get('/auth', (req, res) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(app.oauth.authorize());
+
+  app.use(function(req, res) {
+    res.send('Secret area');
+  });
+})
