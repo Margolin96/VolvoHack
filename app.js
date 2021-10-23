5a3c87b820009bc4c50f92886a7888df45cd6d50
Aleksei Margolin <margolin96@gmail.com>
2021-10-23 18:11:20 +0300

    Fix

--- app.js
+++ app.js
@@ -170,8 +170,6 @@ app.post('/alisa/v1.0/user/unlink', (req, res) => {
 });
 app.get('/alisa/v1.0/user/devices', (req, res) => {
   const { getDevicesList } = require('./alice/aliceHome');
-  console.log(req.query, req.body, req.headers);
-  const list = getDevicesList(req.headers['x-request-id']);
-  list.request_id = req.headers['x-request-id'];
-  res.status(200).send(list);
+  console.log(req.headers['x-request-id']);
+  res.status(200).send(getDevicesList(req.headers['x-request-id']));
 });
