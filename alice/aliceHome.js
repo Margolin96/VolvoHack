ecce3a5c2137bc60bea0dca591f7b24c094ecd06
Aleksei Margolin <margolin96@gmail.com>
2021-10-24 02:25:53 +0300

    Fix

--- alice/aliceHome.js
+++ alice/aliceHome.js
@@ -105,6 +105,10 @@ exports.setOnOffDevicesStates = async (requestId, data) => {
 
   const setStateResults = [];
   for (const d of newDeviceStates) {
+    console.log(JSON.stringify(d, null, 2));
+    if (!d.capabilities) d.capabilities = {};
+    if (!d.capabilities.state) d.capabilities.state = {};
+    if (!d.capabilities.state.value) d.capabilities.state.value = false;
     setStateResults.push(await setOnOffDeviceState(d.id, d.capabilities.state.value));
   }
 
